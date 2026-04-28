93c5b800e664806e252465ece2dcf5fc0a8801dd
BalrajSinghGidda <anonystargamerz@gmail.com>
2026-04-29 02:29:31 +0530

    First Commit

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Dev shell: Python";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python313.withPackages (ps: with ps; [
+          pytest
+          black
+          mypy
+          isort
+          flask
+          sqlalchemy
+          matplotlib
+          memory-profiler
+          flask-sqlalchemy
+          flask-socketio
+          numpy
+          pandas
+          eventlet
+          werkzeug
+          python-docx
+          itsdangerous
+          pip
+          setuptools
+          wheel
+	  euporie
+        ]);
+
+        in {
+        devShells.default = pkgs.mkShell {
+        name = "python-devshell";
+        buildInputs = [
+        pythonEnv
+        pkgs.poetry        
+        pkgs.git
+        pkgs.gdb          
+        pkgs.ripgrep
+        pkgs.nodejs      
+        ];
+        shellHook = ''
+          export VIRTUAL_ENV_DISABLE_PROMPT=1
+          clear
+          echo "📦 Python devshell active — python ${pkgs.python313.version}"
+          echo "Use: python, pip, pytest, black, mypy, poetry"
+          '';
+        };
+    });
+}
+
